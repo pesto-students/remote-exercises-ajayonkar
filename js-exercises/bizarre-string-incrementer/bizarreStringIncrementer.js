fba3f52f042f5cb6d18a991f432b9e8790c7bab1
Ajay Kumar <ajaykumar.ak011@yahoo.com>
2020-05-29 14:54:37 +0530

    Day-7-bizarre-string-incrementer

---
+++ js-exercises/bizarre-string-incrementer/bizarreStringIncrementer.js
@@ -0,0 +1,13 @@
+const bizarreStringIncrementer = string => {
+    const matches = string.match(/\d+$/);
+    if (matches) {
+      const extractLastNumber = matches[0];
+      const newNumber = (+extractLastNumber + 1).toString().padStart(extractLastNumber.length, '0');
+      return string.replace(extractLastNumber, newNumber);
+    }
+    return `${string}1`;
+  };
+
+  console.log("HELLO");
+  
+export { bizarreStringIncrementer };
